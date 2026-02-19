3a00ab18e4ad3fe8dcb7c4ba70c16a946daeae80
james <d0919554120>
2026-02-19 23:18:20 +0800

    Initial Commit

---
+++ myapp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by Aaron James on 2026/2/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
